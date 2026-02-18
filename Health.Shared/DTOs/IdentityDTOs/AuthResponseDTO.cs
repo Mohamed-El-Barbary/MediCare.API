00e6b833522236b7165229aa00c26874e122ec5d
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-02-18 12:53:54 +0200

    feat(identity-module): add identity dtos

---
+++ Health.Shared/DTOs/IdentityDTOs/AuthResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Shared.DTOs.IdentityDTOs
+{
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; } = default!;
+        public DateTime ExpiresAt { get; set; }
+        public UserDTO User { get; set; } = default!;
+    }
+}
---
+++ Health.Shared/DTOs/IdentityDTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Shared.DTOs.IdentityDTOs
+{
+    public record UserDTO(
+        string Id,
+        string Email,
+        string DisplayName,
+        string Role,
+        string AccessToken,
+        DateTime RefreshTokenExpiresOn
+    );
+
+}
