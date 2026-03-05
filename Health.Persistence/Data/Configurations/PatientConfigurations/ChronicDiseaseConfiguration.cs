881a9fdfa2e1864adc36befffe9693bbfab89197
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-03-05 03:10:57 +0200

    feat(patient-module): add Patient entities with configurations and run migrations

---
+++ Health.Persistence/Data/Configurations/PatientConfigurations/ChronicDiseaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Health.Domain.Entities.PatientModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Persistence.Data.Configurations.PatientConfigurations
+{
+    internal class ChronicDiseaseConfiguration : IEntityTypeConfiguration<ChronicDisease>
+    {
+        public void Configure(EntityTypeBuilder<ChronicDisease> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            // Properties
+            builder.Property(c => c.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            // Relationship with PatientChronicDisease
+            builder.HasMany(c => c.PatientChronicDiseases)
+                   .WithOne(pc => pc.ChronicDisease)
+                   .HasForeignKey(pc => pc.ChronicDiseaseId);
+        }
+    }
+}
---
+++ Health.Persistence/Data/Configurations/PatientConfigurations/PatientProfileConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Health.Domain.Entities.PatientModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Persistence.Data.Configurations.PatientConfigurations
+{
+    internal class PatientProfileConfiguration : IEntityTypeConfiguration<PatientProfile>
+    {
+        public void Configure(EntityTypeBuilder<PatientProfile> builder)
+        {
+            // Primary Key
+            builder.HasKey(p => p.Id);
+
+            // Properties
+            builder.Property(p => p.DisplayName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(p => p.Gender)
+                   .IsRequired();
+
+            builder.Property(p => p.DateOfBirth)
+                   .IsRequired();
+
+            // Owned Entity: Address
+            builder.OwnsOne(p => p.Address, a =>
+            {
+                a.Property(ad => ad.Street).HasMaxLength(200);
+                a.Property(ad => ad.City).HasMaxLength(100);
+                a.Property(ad => ad.Country).HasMaxLength(50);
+            });
+
+            // Relationship with PatientChronicDisease
+            builder.HasMany(p => p.PatientChronicDiseases)
+                   .WithOne(pc => pc.Patient)
+                   .HasForeignKey(pc => pc.PatientId);
+        }
+    }
+}
