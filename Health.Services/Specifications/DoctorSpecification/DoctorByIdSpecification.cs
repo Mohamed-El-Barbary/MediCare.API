d85140c588657ad7623d6f1ade3ec932b3ed7bff
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-04-04 18:57:15 +0200

    feat(identity-module): add patient and doctor specifications

---
+++ Health.Services/Specifications/DoctorSpecification/DoctorByIdSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Health.Domain.Entities.DoctorModule;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Health.Services.Specifications.DoctorSpecification
+{
+    public class DoctorByIdSpecification : BaseSpecification<DoctorProfile, int>
+    {
+        public DoctorByIdSpecification(string id)
+            : base(d => d.UserId == id)
+        {
+        }
+    }
+}
