24371688fdf40e3ad859ea298a30f4124efe5d92
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-04-04 18:44:30 +0200

    feat(identity-module): add otp repository, implementation, and entities

---
+++ Health.Domain/Contracts/IOtpRepository.cs
@@ -0,0 +1,17 @@
+﻿using Health.Domain.Entities.IdentityModule;
+using Health.Domain.Entities.IdentityModule.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Domain.Contracts
+{
+    public interface IOtpRepository
+    {
+        Task<OtpEntry?> GetAsync(string email, OtpPurpose purpose);
+        Task SetAsync(string email, OtpEntry entity, OtpPurpose purpose, TimeSpan ttl = default);
+        Task DeleteAsync(string email, OtpPurpose purpose);
+        Task<TimeSpan?> GetRemainingTtlAsync(string email, OtpPurpose purpose);
+
+    }
+}
