ca91a9115deade44799120e38d56fa74b43cdc9b
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-04-05 13:30:21 +0200

    feat(admin-module): introduce Admin MVC project structure

---
+++ Health.Services.Abstraction/AdminModule/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Services.Abstraction.AdminModule
+{
+    public interface IAdminService
+    {
+    }
+}
---
+++ Health.Services/ServicesImplementation/AdminModule/AdminService.cs
@@ -0,0 +1,11 @@
+﻿using Health.Services.Abstraction.AdminModule;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Services.ServicesImplementation.AdminModule
+{
+    public class AdminService : IAdminService
+    {
+    }
+}
