bbba792679b4f7af9bd3771bbd3c36242348b3eb
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-04-20 11:50:26 +0200

    feat(admin-module): register services in DI container and configure app settings

--- AdminDashboard/Program.cs
+++ AdminDashboard/Program.cs
@@ -1,3 +1,19 @@
+using Health.Domain.Contracts;
+using Health.Domain.Entities.IdentityModule;
+using Health.Persistence.Data.DbContexts;
+using Health.Persistence.IdentityData.DbContexts;
+using Health.Persistence.Repositories;
+using Health.Services.Abstraction.AdminModule;
+using Health.Services.Abstraction.IdentityModule;
+using Health.Services.Abstraction.IdentityModuleAbstraction;
+using Health.Services.MappingProfiles;
+using Health.Services.ServicesImplementation;
+using Health.Services.ServicesImplementation.AdminModule;
+using Health.Services.ServicesImplementation.IdentityModule;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
 namespace AdminDashboard
 {
     public class Program
@@ -8,6 +24,34 @@ namespace AdminDashboard
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<HealthCareDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnextion"));
+            });
+
+            builder.Services.AddDbContext<HealthCareIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
+            });
+            builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                return ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConnection")!);
+            });
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<HealthCareIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAutoMapper(typeof(ServiceAssemblyReference).Assembly);
+            builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<IAttachmentService, AttachmentService>();
+            builder.Services.AddScoped<IOtpRepository, OtpRepository>();
+            builder.Services.AddScoped<IOtpService, OtpService>();
+            builder.Services.AddScoped<IEmailService, EmailService>();
 
             var app = builder.Build();
 
@@ -20,14 +64,16 @@ namespace AdminDashboard
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
-
+ 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=AdminAuth}/{action=Index}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
