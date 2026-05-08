ea31507879e97de69e7deaa18d73c33d145cea8b
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-05-08 17:13:11 +0300

    feat(patient-module): add blood type column, emergency contacts, and migration

---
+++ Health.Domain/Entities/PatientModule/EmergencyContact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Domain.Entities.PatientModule
+{
+    public class EmergencyContact
+    {
+        public string ContactName { get; set; } = default!;
+
+        public string PhoneNumber { get; set; } = default!;
+    }
+}
---
+++ Health.Persistence/Data/Migrations/20260508135124_AddEmergencyContactAndBloodTypesColumnsInPatientTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Health.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmergencyContactAndBloodTypesColumnsInPatientTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BloodType",
+                table: "PatientProfiles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmergencyContact_ContactName",
+                table: "PatientProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmergencyContact_PhoneNumber",
+                table: "PatientProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BloodType",
+                table: "PatientProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "EmergencyContact_ContactName",
+                table: "PatientProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "EmergencyContact_PhoneNumber",
+                table: "PatientProfiles");
+        }
+    }
+}
