3ecc667c53c28f6259a61529f2537846c4e54101
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-05-16 14:53:03 +0300

    feat(consultation-module): add database migration and apply updates

--- Health.Persistence/Data/Migrations/20260516114352_AddConsultationModuleTables.Designer.cs
+++ Health.Persistence/Data/Migrations/20260516114352_AddConsultationModuleTables.Designer.cs
@@ -4,6 +4,7 @@ using Health.Persistence.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Health.Persistence.Data.Migrations
 {
     [DbContext(typeof(HealthCareDbContext))]
-    partial class HealthCareDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260516114352_AddConsultationModuleTables")]
+    partial class AddConsultationModuleTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
