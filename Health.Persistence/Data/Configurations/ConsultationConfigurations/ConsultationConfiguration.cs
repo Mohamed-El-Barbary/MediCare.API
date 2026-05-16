d3b1f74944d0a5428d74379e14e13bcb56aba472
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-05-16 18:33:42 +0300

    chore(consultation-module): configure consultation tables and apply database migrations

---
+++ Health.Persistence/Data/Configurations/ConsultationConfigurations/ConsultationConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Health.Domain.Entities.ConsultationModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Persistence.Data.Configurations.ConsultationConfigurations
+{
+    internal class ConsultationConfiguration : IEntityTypeConfiguration<Consultation>
+    {
+        public void Configure(EntityTypeBuilder<Consultation> builder)
+        {
+
+            builder.ToTable("Consultations");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.RoomId)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.Diagnosis)
+                .HasMaxLength(2000);
+
+            builder.Property(c => c.Symptoms)
+                .HasMaxLength(2000);
+
+            builder.HasOne(c => c.Doctor)
+                .WithMany()
+                .HasForeignKey(c => c.DoctorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(c => c.Patient)
+                .WithMany()
+                .HasForeignKey(c => c.PatientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(c => c.RoomId)
+                .IsUnique();
+
+        }
+    }
+}
