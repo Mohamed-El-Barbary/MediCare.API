ce14fb93cd06d306ce899bce7e7074cb4a2a01ee
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-06-02 17:55:58 +0300

    feat(consultation-module): add request and response DTOs for consultation

---
+++ Health.Shared/DTOs/ConsultationDTOs/ConsultationDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Shared.DTOs.ConsultationDTOs
+{
+    public class ConsultationDTO
+    {
+        public int Id { get; set; }
+        public int AppointmentId { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; } = default!;
+        public int PatientId { get; set; }
+        public string PatientName { get; set; } = default!;
+        public string Status { get; set; } = default!;
+        public string Type { get; set; } = default!;
+        public string RoomId { get; set; } = default!;
+        public DateTime ScheduledAt { get; set; } = DateTime.UtcNow;
+        public DateTime? StartedAt { get; set; }
+        public DateTime? EndedAt { get; set; }
+        public string? Symptoms { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Notes { get; set; }
+        public ICollection<PrescriptionDTO> Prescriptions { get; set; }
+            = new List<PrescriptionDTO>();
+
+    }
+}
---
+++ Health.Shared/DTOs/ConsultationDTOs/ConsultationSummaryDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Shared.DTOs.ConsultationDTOs
+{
+    public class ConsultationSummaryDTO
+    {
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; } = default!;
+        public int PatientId { get; set; }
+        public string PatientName { get; set; } = default!;
+        public string Status { get; set; } = default!;
+        public string Type { get; set; } = default!;
+        public DateTime ScheduledAt { get; set; }
+        public DateTime? StartedAt { get; set; }
+        public DateTime? EndedAt { get; set; }
+    }
+}
