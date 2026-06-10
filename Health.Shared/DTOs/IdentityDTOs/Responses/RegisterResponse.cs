8c39b19637f1640a68eb4fb10b0aee162d8ba636
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-06-10 20:17:03 +0300

    refactor(identity-module): restructure DTOs and convert to records

---
+++ Health.Shared/DTOs/IdentityDTOs/Responses/RegisterResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health.Shared.DTOs.IdentityDTOs.Responses
+{
+    public record RegisterResponse(
+        int ProfileId,
+        string Role,
+        string Message,
+        TokenResponse Token
+    );
+}
